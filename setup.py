dd34cefd9365a8c2301f2e60544360db73a6caac
Ted Liefeld <liefeld@broadinstitute.org>
2016-09-27 14:15:07 -0700

    added declaration of pandas dependency and js static assests

--- setup.py
+++ setup.py
@@ -1,2 +1,19 @@
-#genomespace_io  for getting and putting files to GenomeSpace
+from distutils.core import setup
+setup(
+  name = 'genomespaceio',
+  packages = ['genomespaceio'], # this must be the same as the name above
+  version = '0.13',
+  description = 'A library for reading and writing files to GenomeSpace (http://www.genomespace.org)',
+  author = 'Ted Liefeld',
+  author_email = 'jliefeld@cloud.ucsd.edu',
+  url = 'https://github.com/GenomeSpace/genomespace_io', 
+  download_url = 'https://github.com/GenomeSpace/genomespace_io/tarball/0.12', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+  install_requires=['pandas'],
+  package_data={'genomespaceio': ['static/index.js']}
+)
+
+
+
 
