90e9763ba714738340ec9ced49105ea0abd464b9
Ted Liefeld <liefeld@broadinstitute.org>
2016-09-30 10:26:27 -0700

    updating version for re-release to prod

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'genomespaceio',
   packages = ['genomespaceio'], # this must be the same as the name above
-  version = '0.13',
+  version = '0.14',
   description = 'A library for reading and writing files to GenomeSpace (http://www.genomespace.org)',
   author = 'Ted Liefeld',
   author_email = 'jliefeld@cloud.ucsd.edu',
   url = 'https://github.com/GenomeSpace/genomespace_io', 
-  download_url = 'https://github.com/GenomeSpace/genomespace_io/tarball/0.12', # I'll explain this in a second
+  download_url = 'https://github.com/GenomeSpace/genomespace_io/tarball/0.14', # I'll explain this in a second
   keywords = ['testing', 'logging', 'example'], # arbitrary keywords
   classifiers = [],
   install_requires=['pandas'],
